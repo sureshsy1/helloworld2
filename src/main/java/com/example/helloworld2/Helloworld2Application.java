2aa20a8cd727060bdddc88621509d1ad5af0bf1f
Suresh <suresh.sy@gmail.com>
2026-06-14 23:29:44 +0530

    initial checkin

---
+++ src/main/java/com/example/helloworld2/Helloworld2Application.java
@@ -0,0 +1,12 @@
+package com.example.helloworld2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Helloworld2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Helloworld2Application.class, args);
+    }
+}
---
+++ src/test/java/com/example/helloworld2/Helloworld2ApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.helloworld2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Helloworld2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
